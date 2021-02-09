69bac99a204ef45b859162e89742df5c8356d3f9
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-02-09 23:01:15 +0900

    fix station.js

--- routes/v1/station.js
+++ routes/v1/station.js
@@ -7,41 +7,31 @@ const router = express.Router();
 router.get("/", (req, res) => {
 	mongodb.MongoClient.connect("mongodb://127.0.0.1:27017/test20210209").then(client => {
 		const db = client.db("test20210209");
+		let cursor;
 		if (req.query.code != undefined) {
 			// GET http://localhost:3000/api/v1/station?id=<stationCode>
-			db.collection("eki").find({
+			cursor = db.collection("eki").find({
 				stationCode: parseInt(req.query.code)
-			}).toArray((err, result) => {
-				if (err) {
-					res.json({
-						message: "database error",
-						error: err
-					});
-				} else {
-					res.json({
-						message: "success",
-						result: result
-					});
-				}
 			});
 		} else if (req.query.name != undefined) {
 			// GET http://localhost:3000/api/v1/station?name=<stationName>
-			db.collection("eki").find({
+			cursor = db.collection("eki").find({
 				stationName: req.query.name
-			}).toArray((err, result) => {
-				if (err) {
-					res.json({
-						result: err
-					});
-					console.error(err);
-				} else {
-					res.json({
-						result: result
-					})
-				}
-			});
-
+			})
 		}
+		cursor.toArray((err, result) => {
+			if (err) {
+				res.json({
+					message: "database error",
+					error: err
+				});
+			} else {
+				res.json({
+					message: "success",
+					result: result
+				});
+			}
+		})
 		client.close();
 	});
 })
